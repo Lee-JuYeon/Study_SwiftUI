f41b5fe46fdcf813c46e9f7b64c98d210744f2c0
cavss <cavss1118@gmail.com>
2023-02-10 22:08:52 +0900

    #005 CustomTabView

---
+++ Shared/Views/CustomTabView/CustomTabItemModel.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTabItemModel.swift
+//  CustomView (iOS)
+//
+//  Created by C.A.V.S.S on 2023/02/10.
+//
+
+
+import SwiftUI
+
+struct CustomTabItemModel : Hashable{
+    let image: String
+    let title: String
+}
+
+extension CustomTabItemModel {
+    static var CustomTabItemModelList = [
+        CustomTabItemModel(image: "icon_people", title: "tab_mates"),
+        CustomTabItemModel(image: "icon_chat", title: "tab_chat"),
+        CustomTabItemModel(image: "icon_profile", title: "tab_profile")
+    ]
+}
+
