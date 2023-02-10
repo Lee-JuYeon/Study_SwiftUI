20010f7653bc20f8dd0721b698a23c94a672238c
cavss <cavss1118@gmail.com>
2023-02-10 21:43:32 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by C.A.V.S.S on 2023/02/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/CustomViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomViewApp.swift
+//  Shared
+//
+//  Created by C.A.V.S.S on 2023/02/10.
+//
+
+import SwiftUI
+
+@main
+struct CustomViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
